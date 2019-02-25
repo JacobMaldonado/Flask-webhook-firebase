656098e6c4dec0f0d3989abb3d5e0c83309300c6
YeicobXD <jacobmaldonado99@gmail.com>
2019-02-25 12:15:48 -0600

    update: print methods

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = flask.Flask(__name__)
 def home():
     if request.method == 'POST':
         print("peticion " + str(request.form))
+        print("datos" + request.json)
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
 if __name__ == '__main__':
