62dba1e52a7c2f4d851db63682696ebb6e73bc2a
YeicobXD <jacobmaldonado99@gmail.com>
2019-02-25 21:59:44 -0600

    add: function for responses

--- app.py
+++ app.py
@@ -3,9 +3,25 @@ from flask import request, jsonify
 
 PROJECT_ID = "flask-1fa09"
 
-response = {
-      "responseId": "ea3d77e8-ae27-41a4-9e1d-174bd461b68c",
-      "session": "projects/" + PROJECT_ID + "/agent/sessions/88d13aa8-2999-4f71-b233-39cbf3a824a0",
+
+
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    if request.method == 'POST':
+        print("peticion " + str(request.form))
+        print("datos" + str(request.json))
+        return jsonify(generateResponse(request.json.responseId,
+                                        request.json.queryResult.intent.name,
+                                        request.json.queryResult.intent.displayName))
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+def generateResponse(responseId, intentPath, intentName):
+    return {
+      "responseId": responseId,
+      "session": "projects/" + PROJECT_ID + "/agent/sessions/" + responseId,
       "queryResult": {
         "queryText": "user's original query to your agent",
         "parameters": {
@@ -24,7 +40,7 @@ response = {
         ],
         "outputContexts": [
           {
-            "name": "projects/" + PROJECT_ID + "/agent/sessions/88d13aa8-2999-4f71-b233-39cbf3a824a0/contexts/generic",
+            "name": "projects/" + PROJECT_ID + "/agent/sessions/" + responseId + "/contexts/generic",
             "lifespanCount": 5,
             "parameters": {
               "param": "param value"
@@ -32,26 +48,16 @@ response = {
           }
         ],
         "intent": {
-          "name": "projects/" + PROJECT_ID + "/agent/intents/29bcd7f8-f717-4261-a8fd-2d3e451b8af8",
-          "displayName": "Matched Intent Name"
+          "name": intentPath,
+          "displayName": intentName
         },
         "intentDetectionConfidence": 1,
         "diagnosticInfo": {},
-        "languageCode": "en"
+        "languageCode": "es"
       },
       "originalDetectIntentRequest": {}
     }
 
-
-app = flask.Flask(__name__)
-
-@app.route('/', methods=['GET','POST'])
-def home():
-    if request.method == 'POST':
-        print("peticion " + str(request.form))
-        print("datos" + str(request.json))
-        return jsonify(response)
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
 
 if __name__ == '__main__':
