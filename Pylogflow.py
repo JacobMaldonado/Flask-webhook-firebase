dc7c28d0d8d28090c4ae1097952f808b184b31ab
YeicobXD <jacobmaldonado99@gmail.com>
2019-04-07 10:56:54 -0500

    pre upload

---
+++ Pylogflow.py
@@ -0,0 +1,13 @@
+class DialogflowResponse():
+
+    # Constructor of message building class
+    def __init__(self):
+        self.message = ""
+
+    # This mettod appends a message on the response
+    def add(self, msg):
+        self.message += msg
+
+class Message():
+    def card(msg,img):
+        return ""
